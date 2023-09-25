ab4e7bf6e83de31034e424bd77c206484c1db7c5
Daxton-Garner <dgarner@mines.edu>
2023-09-25 12:40:39 -0600

    Test

--- Mini Project/miniprojControl/miniprojControl.ino
+++ Mini Project/miniprojControl/miniprojControl.ino
@@ -1,10 +1,26 @@
+int pin10 = 10;
+int pin9 = 9;
+int pin8 = 8;
+int pin7 = 7;
+int pin4 = 4;
+
+int value = 75;
+
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(pin10, OUTPUT);
+  pinMode(pin9, OUTPUT);
+  pinMode(pin8, OUTPUT);
+  pinMode(pin7, OUTPUT);
+  pinMode(pin4, OUTPUT);
 
+  digitalWrite(pin4, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  analogWrite(pin9, value);
+  analogWrite(pin10, value);
 
+  digitalWrite(pin7, HIGH);
+  digitalWrite(pin8, LOW);
 }
