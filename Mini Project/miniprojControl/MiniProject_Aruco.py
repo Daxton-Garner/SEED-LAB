10de7b43dd556e8de91f961c163768ea56d9e648
Jordie <82742888+JordieW@users.noreply.github.com>
2023-09-25 09:50:18 -0900

    Added aruco detection files

---
+++ Mini Project/miniprojControl/MiniProject_Aruco.py
@@ -0,0 +1,101 @@
+# Jordie Weber
+# SEED Lab - Computer Vission Assignment 2a
+
+# A lot of imports
+import time
+import board
+import numpy as np
+import cv2 as cv
+from cv2 import aruco
+from time import sleep
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+# lcd initialization, set columns/rows, color, text direction
+lcd_columns = 16
+lcd_rows = 2
+i2c = board.I2C()
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+lcd.clear()
+lcd.color = [50, 0, 50]
+lcd.text_direction = lcd.LEFT_TO_RIGHT
+outputString = " "
+previousOutput = "."
+
+# Disctionary for aruco generation and detection
+myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
+
+# This code generates markers, uncomment to display a marker
+#myAruco = aruco.GridBoard((1,1),4.0,1.0,myDict)
+#myArucoImg = myAruco.generateImage((500,500))
+#cv.imshow("myArucoImg",myArucoImg)
+#cv.imwrite("Aruco.jpg", myArucoImg)
+
+# Capture from the camera plugged into the Pi
+capture = cv.VideoCapture(0)
+
+# Set dimensions of the capture and find the center of the window
+capWidth = 640
+capHeight = 480
+xCapCent = capWidth/2
+yCapCent = capHeight / 2
+capture.set(cv.CAP_PROP_FRAME_WIDTH, capWidth)
+
+
+# Continue this loop until the break at the bottom
+while True:
+
+    # Read from the camera capture, set the color to greyscale, run aruco detection
+    ret, frame = capture.read()     
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    corners,ids,rejected = aruco.detectMarkers(gray,myDict)
+
+    # Make an overlay for detection
+    overlay = cv.cvtColor(gray, cv.COLOR_GRAY2RGB)
+    overlay = aruco.drawDetectedMarkers(overlay,corners,borderColor = 4)
+
+    # Enter this code if a marker is found
+    if not ids is None:
+
+        # Calculate where the center of the marker is on the screen
+        cornersArray = np.array(corners)[0][0]
+        xMarkCent = (cornersArray[0][0] + cornersArray[1][0] + cornersArray[2][0] + cornersArray[3][0]) / 4 
+        yMarkCent = (cornersArray[0][1] + cornersArray[1][1] + cornersArray[2][1] + cornersArray[3][1]) / 4
+
+        # Compare center of marker to center of screen to locate marker in capture
+        if (xMarkCent < xCapCent) and (yMarkCent < yCapCent):
+            outputString = "NW"
+        elif (xMarkCent < xCapCent) and (yMarkCent > yCapCent):
+            outputString = "SW"
+        elif (xMarkCent > xCapCent) and (yMarkCent < yCapCent):
+            outputString = "NE"
+        else:
+            outputString = "SE"
+
+        # Edit the overlay display to outline the marker
+        ids = ids.flatten()
+        for (outline, id) in zip(corners, ids):
+            markerCorners = outline.reshape((4,2))
+            overlay = cv.putText(overlay, str(id), (int(markerCorners[0,0]),int(markerCorners[0,1])-15),cv.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2)
+    else:
+        # If a marker is not found
+        outputString = "No markers found"
+
+    
+    # Show the overlay
+    cv.imshow("overlay", overlay)
+
+    # Write the output string to the lcd screen, keep track of chnages in output
+    if outputString != previousOutput: lcd.clear()
+    lcd.message = outputString
+    previousOutput = outputString
+
+    # press q key to exit out of the program
+    k = cv.waitKey(1) & 0xFF
+    if k == ord('q'):
+        break
+
+# Once out of while, release the capture and destroy windows
+capture.release()
+cv.destroyAllWindows()
+
+                                 
