57849f20410e42d05e41047e548194adc97dc8ea
Daxton-Garner <dgarner@mines.edu>
2023-10-09 13:46:51 -0600

    Straight Line Updated

--- Demo1/StraightLineControl/StraightLineControl.ino
+++ Demo1/StraightLineControl/StraightLineControl.ino
@@ -9,18 +9,22 @@
 #define B2 5 //Left Motor Encoder
 #define A1 3 //Right Motor Encoder
 #define B1 6 //Right Motor Encoder
-#define PWMPIN 9  //Left Motor PWM
-#define PWMPIN 10  //Right Motor PWM
-#define DIRPIN 7  //Left Motor Direction
-#define DIRPIN 8  //Right Motor Direction
+#define PWMPINL 9  //Left Motor PWM
+#define PWMPINR 10  //Right Motor PWM
+#define DIRPINL 7  //Left Motor Direction
+#define DIRPINR 8  //Right Motor Direction
 #define PIN4 4
 #define MY_ADDR 8
 
 bool debug = 0;
 float Kp = 4; // 1.7 was Our gain we found in our insitiall simulink
-float Kp_pos = 5; // The proportional gain
-float Ki_pos = 0.75; // the intergral gain
+float Kp_pos = 1; // The proportional gain
+float Ki_pos = 2.5; // the intergral gain
 
+
+float targetdistft = 7;  //Circumfrance of wheel 47.1 cm or 0.471 m
+float targetdistm;
+float wheelCir = 0.471;
 float targetRad;
 float desired_speed;
 float integral_error = 0;
@@ -43,9 +47,9 @@ float last_pos_rad;
 
 void A2Change() {                            //Interrupt handler for pin A
   if (digitalRead(A2) == digitalRead(B2)) {  //Encoder direction check
-    encoderCountsL += 2;                      //Adjusts for changes made during periodic reporting
+    encoderCountsL -= 2;                      //Adjusts for changes made during periodic reporting
   } else {
-    encoderCountsL -= 2;
+    encoderCountsL += 2;
   }
   lastA2 = digitalRead(A2);  //Reset states
   lastB2 = digitalRead(B2);
@@ -53,9 +57,9 @@ void A2Change() {                            //Interrupt handler for pin A
 
 void A1Change() {                            //Interrupt handler for pin A
   if (digitalRead(A1) == digitalRead(B1)) {  //Encoder direction check
-    encoderCountsR += 2;                      //Adjusts for changes made during periodic reporting
+    encoderCountsR -= 2;                      //Adjusts for changes made during periodic reporting
   } else {
-    encoderCountsR -= 2;
+    encoderCountsR += 2;
   }
   lastA1 = digitalRead(A1);  //Reset states
   lastB1 = digitalRead(B1);
@@ -73,15 +77,18 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(A2), A2Change, CHANGE);  // attach interrupt handler
   attachInterrupt(digitalPinToInterrupt(A1), A1Change, CHANGE);  // attach interrupt handler
 
-  pinMode(PWMPIN, OUTPUT);
-  pinMode(DIRPIN, OUTPUT);
+  pinMode(PWMPINL, OUTPUT);
+  pinMode(DIRPINL, OUTPUT);
+
+  pinMode(PWMPINR, OUTPUT);
+  pinMode(DIRPINR, OUTPUT);
 
   pinMode(PIN4, OUTPUT);
   digitalWrite(PIN4, HIGH);
 
-  Serial.begin(115200);
-  last_time_ms = millis();  // set up sample time variable
-  start_time_ms = last_time_ms;
+  targetdistm = 0.3048 * targetdistft;
+  
+  targetRad = (2*PI) * (targetdistm / wheelCir);
 }
 
 void loop() {
@@ -89,13 +96,15 @@ void loop() {
 
   current_time = (float)(last_time_ms - start_time_ms) / 1000; //Current time (sec)
 
-  pos_rad = 2 * PI * (float)encoderCounts / 3200.0;  //Convert positions from steps to radians
+  pos_rad = 2 * PI * (float)encoderCountsL / 3200.0;  //Convert positions from steps to radians
 
   ang_velocity = (pos_rad - last_pos_rad) / (current_time - last_time); //Calculate angular velocity
 
 
   pos_error = targetRad - pos_rad;  //Calc position error
   integral_error = integral_error + pos_error * ((float)desired_Ts_ms / 1000); //Discrete integration (just a sum)
+  Serial.println(integral_error);
+  if(integral_error > 1.5) {integral_error = 1.5;}
   desired_speed = Kp_pos * pos_error + Ki_pos * integral_error; //Calc desired speed
   error = desired_speed - ang_velocity; //Velocity error
   value = Kp * error; //Final PWM value calculation
@@ -107,15 +116,19 @@ void loop() {
     motor_dir = 1;
   }
   
-  value = value * 255 / 8; //Scale control loop output to PWM
-  if (value > 255) { value = 255; } //Cap PI controller output at 255
+  value = value * 32; //Scale control loop output to PWM
+  if (value > 100) { value = 100; } //Cap PI controller output at 255
 
 
-  analogWrite(PWMPIN, value); //Control direction pin
+  analogWrite(PWMPINL, value); //Control direction speed
+  analogWrite(PWMPINR, value); //Control direction speed
+
   if (motor_dir) {
-    digitalWrite(DIRPIN, HIGH);
+    digitalWrite(DIRPINL, LOW);
+    digitalWrite(DIRPINR, HIGH);
   } else {
-    digitalWrite(DIRPIN, LOW);
+    digitalWrite(DIRPINL, HIGH);
+    digitalWrite(DIRPINR, LOW);
   }
 
   if (debug) {
