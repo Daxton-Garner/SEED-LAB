144aa14e0a71a1fb2e01a305b9faad28656ec2ab
baileyoteri <146249213+baileyoteri@users.noreply.github.com>
2023-10-25 14:17:16 -0600

    Start Demo 2

---
+++ Demo2/Demo1WithComms/Demo1WithComms.ino
@@ -0,0 +1,231 @@
+//Description: This code is what controls the wheel to turn to the specific angle requested using the
+//             motors current position and some PI controllers to manage the rise time and overshoot.
+//Hardware:    You will need an Arduino Uno connected to a motor, a voltage monitor, and Raspberry Pi.
+//             The motor should have a wheel attached and the Raspberry Pi will need a camera.
+
+
+#include <Wire.h>   //This is our Pi communication library
+#define A2 2        //Left Motor Encoder
+#define B2 5        //Left Motor Encoder
+#define A1 3        //Right Motor Encoder
+#define B1 6        //Right Motor Encoder
+#define PWMPINL 9   //Left Motor PWM
+#define PWMPINR 10  //Right Motor PWM
+#define DIRPINL 7   //Left Motor Direction
+#define DIRPINR 8   //Right Motor Direction
+#define PIN4 4
+#define MY_ADDR 8
+
+// START BAILEY ADD
+volatile uint8_t instruction[32]={0};
+volatile uint8_t msgLength = 0;
+volatile uint8_t reply = 0;
+// END BAILEY ADD
+
+bool debug = 0;
+bool turnComplete = 0;
+int stallCount = 0;
+float Kp = 4;        // 1.7 was Our gain we found in our insitiall simulink
+float Kp_pos = 1;    // The proportional gain
+float Ki_pos = 2.5;  // the intergral gain
+
+float targetdeg = 270.0;
+float targetdistft = 3;  //Circumfrance of wheel 47.1 cm or 0.471 m
+
+float targetdistm;
+//float targetrotrad = targetdeg * 3.14159 / 180.0;
+float targetrotm = (targetdeg / 360.0) * (0.685 * PI);  //Wheel dist is 27.1cm aka 0.271m. Used to be a 0.5 here but I dropped it becuase it did exactly half what I asked
+
+float wheelCir = 0.471;
+float targetRad;
+float desired_speed[2];
+float integral_error[2] = { 0, 0 };
+float pos_error[2] = { 0, 0 };
+float error[2] = { 0, 0 };
+
+int value[2];
+int motor_dir[2] = { 1, 1 };
+long encoderCounts[2];
+int lastA2, lastB2, lastA1, lastB1;
+unsigned long desired_Ts_ms = 10;  // desired sample time in milliseconds
+unsigned long last_time_ms;
+unsigned long start_time_ms;
+float current_time;
+float last_time;
+
+float ang_velocity[2];
+float last_pos_rad[2];
+
+void A2Change() {                            //Interrupt handler for pin A
+  if (digitalRead(A2) == digitalRead(B2)) {  //Encoder direction check
+    encoderCounts[0] -= 2;                   //Adjusts for changes made during periodic reporting
+  } else {
+    encoderCounts[0] += 2;
+  }
+  lastA2 = digitalRead(A2);  //Reset states
+  lastB2 = digitalRead(B2);
+}
+
+void A1Change() {                            //Interrupt handler for pin A
+  if (digitalRead(A1) == digitalRead(B1)) {  //Encoder direction check
+    encoderCounts[1] += 2;                   //Adjusts for changes made during periodic reporting
+  } else {
+    encoderCounts[1] -= 2;
+  }
+  lastA1 = digitalRead(A1);  //Reset states
+  lastB1 = digitalRead(B1);
+}
+
+void setup() {
+  Serial.begin(115200);     // Set the baud rate fast so that we can display the results
+  last_time_ms = millis();  // set up sample time variable
+  start_time_ms = last_time_ms;
+  pinMode(A1, INPUT_PULLUP);  //Set inputs and pullups
+  pinMode(B1, INPUT_PULLUP);
+  pinMode(A2, INPUT_PULLUP);  //Set inputs and pullups
+  pinMode(B2, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(A2), A2Change, CHANGE);  // attach interrupt handler
+  attachInterrupt(digitalPinToInterrupt(A1), A1Change, CHANGE);  // attach interrupt handler
+
+  pinMode(PWMPINL, OUTPUT);
+  pinMode(DIRPINL, OUTPUT);
+
+  pinMode(PWMPINR, OUTPUT);
+  pinMode(DIRPINR, OUTPUT);
+
+  pinMode(PIN4, OUTPUT);
+  digitalWrite(PIN4, HIGH);
+
+  //START BAILEY ADDED
+
+  //We want to control the built-in LED (pin 13)
+  pinMode (LED_BUILTIN, OUTPUT);
+  //initiallize I2C
+  Wire.begin(MY_ADDR);
+  //set callbacks for I2C interrupts 
+  Wire.onReceive (receive);
+  //send data back, send back length of message received
+  Wire.onRequest(request);
+
+  //END BAILEY ADD
+
+  targetRad = (2 * PI) * (targetrotm / wheelCir);
+}
+
+void loop() {
+  float pos_rad[2];
+
+  current_time = (float)(last_time_ms - start_time_ms) / 1000;  //Current time (sec)
+
+  for (int i = 0; i < 2; i++) {
+    pos_rad[i] = 2 * PI * (float)encoderCounts[i] / 3200.0;     //Convert positions from steps to radians
+    //if (!turnComplete && i == 1) { pos_rad[i] = pos_rad[i]*(-1);} //Make one wheel go backwards during turn
+
+    ang_velocity[i] = (pos_rad[i] - last_pos_rad[i]) / (current_time - last_time);  //Calculate angular velocity
+    if (!turnComplete && i == 1) { pos_error[i] = (-1)*targetRad - pos_rad[i];   }
+    else { pos_error[i] = targetRad - pos_rad[i]; }
+
+    integral_error[i] = integral_error[i] + pos_error[i] * ((float)desired_Ts_ms / 1000);  //Discrete integration (just a sum)
+    //Serial.println(integral_error[i]);
+    if (integral_error[i] > 1.5) { integral_error[i] = 1.5; }
+    if (integral_error[i] < -1.5) { integral_error[i] = -1.5; }
+    desired_speed[i] = Kp_pos * pos_error[i] + Ki_pos * integral_error[i];  //Calc desired speed
+    error[i] = desired_speed[i] - ang_velocity[i];                          //Velocity error
+    value[i] = Kp * error[i];                                               //Final PWM value calculation
+
+    if (value[i] < 0) {  //Translate negative pwm value to reversed motor direction
+      value[i] *= -1;
+      motor_dir[i] = 0;
+    } else {
+      motor_dir[i] = 1;
+    }
+
+    value[i] = value[i] * 32;                //Scale control loop output to PWM
+    if (value[i] > 100) { value[i] = 100; }  //Cap PI controller output at 255
+  }
+
+  analogWrite(PWMPINL, value[0]);  //Control direction speed
+  analogWrite(PWMPINR, value[1]);  //Control direction speed
+
+  if (motor_dir[0]) {
+    digitalWrite(DIRPINL, LOW);
+  } else {
+    digitalWrite(DIRPINL, HIGH);
+  }
+  if (motor_dir[1]) {
+    digitalWrite(DIRPINR, HIGH);
+  } else {
+    digitalWrite(DIRPINR, LOW);
+  }
+
+  if (debug) {
+    Serial.print(current_time);
+    Serial.print("\t");
+    Serial.print(value[0]);
+    Serial.print("\t");
+    Serial.print(encoderCounts[0]);
+    Serial.print("\t");
+    Serial.print(value[1]);
+    Serial.print("\t");
+    Serial.print(encoderCounts[1]);
+    Serial.println("");
+  }
+
+  if (ang_velocity[0] + ang_velocity[1] < 0.01) {
+    stallCount++;
+  }
+  else {
+    stallCount = 0;
+  }
+  if ((stallCount >= 500 || targetdeg == 0)&& !turnComplete) {
+    Serial.println("Turn Complete");
+    turnComplete = 1;
+    encoderCounts[0] = 0;
+    encoderCounts[1] = 0;
+    integral_error[0] = 0;
+    integral_error[1] = 0;
+    pos_rad[0] = 0;
+    pos_rad[1] = 0;
+    targetdistm = 0.3048 * targetdistft;
+    targetRad = (2 * PI) * (targetdistm / wheelCir);
+
+  }
+
+  last_pos_rad[0] = pos_rad[0];
+  last_pos_rad[1] = pos_rad[1];
+
+  while (millis() < last_time_ms + desired_Ts_ms) {
+    //wait until desired time passes to go top of the loop
+  }
+  last_time_ms = millis();
+  last_time = current_time;
+
+
+    // START BAILEY ADD 
+  if (msgLength != 0){
+    digitalWrite(LED_BUILTIN,instruction[0]);
+    printReceived();
+    msgLength =0;
+  }
+  // END BAILEY ADD  
+}
+
+//START BAILEY ADD
+void printReceived(){
+  Serial.print("From Rasberry PI:");
+  Serial.println(instruction[0]);
+  Serial.println("");
+}
+void receive (){
+  while(Wire.available()){
+    instruction[msgLength] = Wire.read(); 
+    msgLength++;
+  }
+}
+
+void request(){
+  Wire.write(69);
+  reply = millis();
+}
+//END BAILEY ADD
---
+++ Demo2/Demo1WithCommsPI.py
@@ -0,0 +1,148 @@
+# SEED Lab - Demo 1 - Computer Vission
+# Purpose: Determine the angle of an Aruco marker compared to the center of the camera
+# Method: Capture an image using the camera. If an Aruco marker is detected, display the andle on
+#           the LCD screen. Calculate the angle by determining the distance of the marker from the
+#           camera and the left to right distance from the center of the capture. Then take the
+#           tangent of a ratio of those values to detemrmine theangle the marker is at.
+# Required hardware: LCD screen, camera
+
+
+# Initialization
+import time
+import board
+import numpy as np
+import cv2 as cv
+import threading
+import queue
+import math
+
+from cv2 import aruco
+from time import sleep
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+from smbus2 import SMBus
+from random import random
+
+# Variable creation
+q = queue.Queue()
+change = False
+ThetaLast = -1
+
+#START BAILEY ADD 
+ARD_ADDR = 8
+offset = 1
+#initialize I2C bus
+i2c = board.I2C()
+i2c = SMBus(1)
+#END BAILEY ADD
+
+# Disctionary for aruco generation and detection
+myDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_50)
+
+# This code generates markers, uncomment to display a marker
+#myAruco = aruco.GridBoard((1,1),4.0,1.0,myDict)
+#myArucoImg = myAruco.generateImage((500,500))
+#cv.imshow("myArucoImg",myArucoImg)
+#cv.imwrite("Aruco.jpg", myArucoImg)
+
+# Capture from the camera plugged into the Pi
+capture = cv.VideoCapture(0)
+sleep (0.1)
+
+# Set dimensions of the capture and find the center of the window
+capWidth = 640
+xCapCent = capWidth/2
+markWidthCM = 5
+capture.set(cv.CAP_PROP_FRAME_WIDTH, capWidth)
+
+i2c = SMBus(1)
+
+# Threading function for LCD screen - improves I2C speed 
+def myFunction():
+    # LCD screen initialization
+    lcd_columns = 16
+    lcd_rows = 2
+    i2c = board.I2C()
+    lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+    lcd.color = [50, 0, 50]
+    lcd.clear()
+    # Loop to find if LCD screen needs to change output
+    while True:
+        if change:
+            gotSomething = q.get()
+            if(gotSomething != 0):
+                if (Theta < 0):
+                    lcd.color = [0, 150, 50]
+                else:
+                    lcd.color = [50, 0, 50]
+                ToPrint = str(Theta)
+                lcd.message = ToPrint
+            else:
+                lcd.color = [255,0,0]
+                lcd.message = "\n"
+        # START BAILEY ADD
+        i2c = SMBus(1)
+        i2c.write_byte_data(ARD_ADDR, offset, 55)  
+        reply = i2c.read_byte_data(ARD_ADDR, offset)
+        print("Received from Arduino: "+str(reply))
+        lcd.message = ("From Arduino\n" +str(reply))
+        lcd.color = [100, 0 ,0]
+        # END BAILEY ADD 
+myThread = threading.Thread(target=myFunction,args=())
+myThread.start()
+
+# Continue this loop until the break at the bottom
+while True:
+
+    # Read from the camera capture, set the color to greyscale, run aruco detection
+    ret, frame = capture.read()     
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    corners,ids,rejected = aruco.detectMarkers(gray,myDict)
+
+    # Make an overlay for detection
+    overlay = cv.cvtColor(gray, cv.COLOR_GRAY2RGB)
+    overlay = aruco.drawDetectedMarkers(overlay,corners,borderColor = 4)
+
+    # Enter this code if a marker is found
+    if not ids is None:
+
+        # Calculate where the center of the marker is on the screen (x coordinate)
+        cornersArray = np.array(corners)[0][0]
+        xMarkCent = (cornersArray[0][0] + cornersArray[1][0] + cornersArray[2][0] + cornersArray[3][0]) / 4 
+
+        # Calculate theta
+        center2CenterPix = xCapCent - xMarkCent # Center of capture to center of marker in pixels
+        markWidthPix = (abs(cornersArray[0][0] - cornersArray[1][0]) + abs(cornersArray[2][0] - cornersArray[3][0]))/2
+
+        distance = 4161.416*(markWidthPix)**(-1.055)    # Marker dstance from camera, centimeters; power interpolation
+        left2Right = markWidthCM * center2CenterPix / markWidthPix  # Left to right distance from center, cm
+        theta = 90 - ((math.atan2(distance, left2Right)) * 180 / math.pi)     # Angle from center, degrees
+
+        # Round theta to 2 decimal points
+        Theta = round(theta, 2)
+
+        # Loop to see if Theta has changed or not
+        if (Theta != ThetaLast):
+            q.put(Theta)
+            change = True
+        else:
+            change = False
+            ThetaLast = Theta
+            q.put(0)
+
+        # Edit the overlay display to outline the marker
+        ids = ids.flatten()
+        for (outline, id) in zip(corners, ids):
+            markerCorners = outline.reshape((4,2))
+            overlay = cv.putText(overlay, str(id), (int(markerCorners[0,0]),int(markerCorners[0,1])-15),cv.FONT_HERSHEY_SIMPLEX,0.5,(255,0,0),2)
+
+    # Show the overlay
+    cv.imshow("overlay", overlay)
+
+    # press q key to exit out of the program
+    k = cv.waitKey(1) & 0xFF
+    if k == ord('q'):
+        break
+
+# Once out of while, release the capture and destroy windows
+capture.release()
+cv.destroyAllWindows()
